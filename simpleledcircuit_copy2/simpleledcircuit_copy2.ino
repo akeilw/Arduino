2745196be2d2ac79a94863b87fb336d0aa7e04f1
akeilw <akeilwahee@gmail.com>
2026-08-17 17:43:26 -0600

    Update simpleledcircuit_copy2.ino

--- simpleledcircuit_copy2/simpleledcircuit_copy2.ino
+++ simpleledcircuit_copy2/simpleledcircuit_copy2.ino
@@ -4,25 +4,25 @@ int yellowOnTime=1000;
 int yellowOffTime=1000;
 int redOnTime=1000;
 int redOffTime=1000;
-int numYellowBlink=6;
-int numRedBlink=7;
+int numYellowBlink; 
+int numRedBlink;  
 String yelMessage="Yellow Blinking";  // Declare string variable
 String redMessage="Red Blinking";   
 void setup() {
-  Serial.begin(74880);
-  delay(2000);
-  String oh1= "Only In ";   // Declare string variable oh1 and assign a value
-  String oh2= "Ohio";    // Declare string variable oh2 and assign a value
-  String oh3;           // Declare string variable oh3
-  oh3=oh1 + oh2;        // Concatenating strings oh1 and oh2 into oh3
-  Serial.print(oh3);
+  Serial.begin(921600);
   pinMode(yellowPin, OUTPUT);
   pinMode(redPin, OUTPUT);
 }
 
 void loop() {
 
-Serial.println("    ");
+Serial.println("How many times do you want the yellow LED to blink? ");
+while (Serial.available()==0) { };   // Wait {} til User Inputs Data -> key note == means test for value
+numYellowBlink = Serial.parseInt();   // Serial means input to serial port, parse means read, Int means an integer is the expected variable
+Serial.println("How many times do you want the red LED to blink? ");
+while (Serial.available()==0) { };
+numRedBlink = Serial.parseInt();   
+
 Serial.println(yelMessage);
 for (int i=1; i<=numYellowBlink; i=i+1) {
   Serial.print("    Blink #:  ");
@@ -44,5 +44,7 @@ for (int i=1; i<=numRedBlink; i=i+1) {
   delay(redOffTime);
 
 }
+
+Serial.println("    ");
 
 }
