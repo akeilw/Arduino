d1da112738e2de197a0d1a8bf31fb83317a2851e
akeilw <akeilwahee@gmail.com>
2026-08-21 22:15:09 -0600

    Update potentiometer.ino

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,17 +1,17 @@
-int potPen=A0;
-int readValue;
+int potPen=A0;  //declare potPen as analog pin
+int readValue;   //declare readValue as a variable
 float lVoltage;
 float rVoltage;
 
 void setup() {
-pinMode(potPen, INPUT);
-Serial.begin(1200);
+pinMode(potPen, INPUT); // set potPen as an input to send signals to code
+Serial.begin(1200); // begin serial port
 }
 
 void loop() {
-  readValue = analogRead(potPen);
-  lVoltage = readValue/151.6;
-  rVoltage = abs(5 - lVoltage);
+  readValue = analogRead(potPen); // set readValue to the value of the raw input signals
+  lVoltage = readValue/151.6; //find voltage through this equation
+  rVoltage = abs(5 - lVoltage);  //find voltage through other end of potentiometer
   Serial.print("Left: ");
   Serial.print(lVoltage);
   Serial.print("V Right: ");
